ff32458a9f6866b626147b8c382c477a2b1ad002
Tamas Nagy <thomassocska@yahoo.com>
2018-03-10 18:38:13 +0100

    Simplify params for make_id

--- R/make_id.R
+++ R/make_id.R
@@ -2,27 +2,28 @@
 #'
 #' Select the best id based on a pre-defined hierarchy, and keep only that for the article
 #' @name make_id
-#' @usage make_id(df, .id_hierarchy = id_hierarchy)
+#' @usage make_id(df, identifier = c("doi","pmid","psyid"))
 #' @param df data frame that has identifiers
-#' @param .id_hierarchy data frame with identifier<chr>, and rank<num> columns that define the rank (importance) of an identifier. Smaller number means more important
+#' @param identifier a vector of identifiers <chr> in decreasing order by importance
 #' @return A data frame without individual id columns, and with new identifier and id columns that contain the best available identifier and the id, respectively
 #' @examples
-#' make_id(df)
+#' make_id(df, c("doi","pmid","psyid"))
 
 library(tidyr)
 library(dplyr)
-id_hierarchy <- tibble(identifier = c("doi","pmid","psyid","eid","pq_id","no_id"),
-                       id_rank = c(1, 2, 3, 4, 5, 6))
 
-make_id <- function(df, .id_hierarchy = id_hierarchy){
+make_id <- function(df,
+                    identifier = c("doi","pmid","psyid")){
     # Stop if there are no valid identifiers in the data frame
-    stopifnot(names(df) %>%
-                  intersect(id_hierarchy$identifier) %>%
-                  length() > 0)
+    stopifnot(is.data.frame(df),
+              length(intersect(names(df), identifier)) > 0)
+    # Create a tbl of identifiers for merging
+    id_hierarchy <- tibble(identifier = identifier,
+                           id_rank = seq(1L, length(identifier), 1L))
 
     df %>%
         # Gather all available identifiers
-        gather(identifier, id, intersect(names(.), id_hierarchy$identifier)) %>%
+        gather(identifier, id, intersect(names(.), identifier)) %>%
         drop_na(id) %>%
         # Get information about the rank of the identifier
         left_join(id_hierarchy, by = "identifier") %>%
@@ -36,3 +37,8 @@ make_id <- function(df, .id_hierarchy = id_hierarchy){
         select(-id_rank, -best_id) %>%
         select(identifier, id, source, everything())
 }
+
+
+
+
+
