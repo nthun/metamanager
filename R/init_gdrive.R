1786af9e0f6497408967a09625ed85dd6b1ab0c9
Tamas Nagy <thomassocska@yahoo.com>
2018-05-13 19:40:41 +0200

    Add init_gdrive() function (Could be made simpler if recursive folder structures will be made possible in the gdrive API)

---
+++ R/init_gdrive.R
@@ -0,0 +1,52 @@
+#' Create folder structure on goodle drive
+#'
+#' Create a folder structure (default or custom) on google drive for a meta-analysis project
+#' @name init_gdrive
+#' @usage init_gdrive(gdrive_path = NULL, folders = folders)
+#' @param gdrive_path a full (new) gdrive path <chr>, preferably with / at the end
+#' @param folders a vector <chr> of folder names to create in path
+#' @return no output, this function exerts a side-effect
+#' @examples
+#' # Creating default folder structure
+#' init_gdrive("research/meta-analysis/")
+#' # Creating custom folder structure
+#' init_gdrive("research/meta-analysis/", c("screening", "extract"))
+
+source("R/create_path_structure.R")
+
+init_gdrive <-
+    function(gdrive_path = NULL,
+             folders = c(
+                 "literature_search/",
+                 "screening/",
+                 "screening_consensus/",
+                 "fulltext/",
+                 "extraction/",
+                 "extraction_consensus/"
+             )) {
+
+    stopifnot(length(gdrive_path) > 0)
+
+    # Run listing safely, so if fails, does not stop the function
+    safe_drive_ls <- purrr::safely(googledrive::drive_ls)
+    drive_list <- safe_drive_ls(googledrive::gdrive_path)
+
+    # Stop if the path contains files and can't overwrite
+    stopifnot((nrow(drive_list$result) > 0))
+
+    # If folder does not exist, create it
+    # if (!is.null(drive_list$error)) googledrive::drive_mkdir(gdrive_path)
+
+    all_path <- paste(gdrive_path, folders, sep = "/")
+    all_path <- create_path_structure(all_path)
+    purrr::walk(all_path, ~googledrive::drive_mkdir(name = .x))
+
+}
+
+
+
+
+
+
+
+
