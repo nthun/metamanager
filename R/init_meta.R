70aa09505059f26804e9fa1a42987c287d7fe828
Tamas Nagy <thomassocska@yahoo.com>
2018-05-14 00:20:50 +0200

    Add init_meta() !Currently has a bug!

---
+++ R/init_meta.R
@@ -0,0 +1,16 @@
+#' Create a folder structure and a team file on goodle drive
+#'
+#' Create a folder structure on google drive for a meta-analysis project and a template team file in it that opens in the browser
+#' @name init_meta
+#' @usage init_meta(gdrive_path = NULL)
+#' @param gdrive_path a full (new) gdrive path <chr>, preferably with / at the end
+#' @return no output, this function exerts a side-effects
+#' @examples
+#' # Creating default folder structure and team file
+#' init_meta("research/meta")
+#' TODO: A bug makes the function to initialize the gdrive_path directory twice, therefor it breaks halfway
+
+init_meta <- function(gdrive_path){
+    init_gdrive(gdrive_path)
+    init_meta(gdrive_path)
+}
