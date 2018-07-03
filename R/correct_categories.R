d0ce0110c57691d5e67d2937575f44de507948ab
nthun <thomassocska@yahoo.com>
2018-07-04 00:22:45 +0200

    Add parametrization with defaults instead of preset params

--- R/correct_categories.R
+++ R/correct_categories.R
@@ -5,21 +5,24 @@
 #' @usage correct_categories(to_be_corrected, correct_terms)
 #' @param to_be_corrected vector containing the strings to be corrected
 #' @param correct_terms string vector containing the correct terms
+#' @param max_dist parameter passed down to stringdist::amatch() with a default
+#' @param method parameter passed down to stringdist::amatch() with a default
+#' @param ... further parameters to be passed down to stringdist::amatch()
 #' @return A corrected string vector that can only contain the correct terms
 #' @examples
 #' articles %>%
 #'  mutate(correct_reason = correct_categories(reason, read_lines("vg-meta-reasons.txt")))
 
-correct_categories <- function(to_be_corrected, correct_terms) {
+correct_categories <- function(to_be_corrected, correct_terms, max_dist = 2, method = "cosine", ...) {
     stopifnot(is.character(to_be_corrected),
               is.character(correct_terms))
 
     correct_terms[stringdist::amatch(
-                    to_be_corrected %>%
-                        stringr::str_to_lower() %>%
-                        stringr::str_replace("\\?", NA_character_),
-                    correct_terms,
-                    maxDist = 2,
-                    method = "cosine"
-    )]
+                        stringr::str_to_lower(to_be_corrected),
+                        stringr::str_to_lower(correct_terms),
+                        maxDist = max_dist,
+                        method = method,
+                        ...
+                     )
+                  ]
 }
