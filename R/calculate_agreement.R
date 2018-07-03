56615e1ead09a37c235d3b91f76a04a9566b2b78
nthun <thomassocska@yahoo.com>
2018-07-04 00:08:57 +0200

    Add explicit namespace (not sure it works now)

--- R/calculate_agreement.R
+++ R/calculate_agreement.R
@@ -2,90 +2,85 @@
 #'
 #' Calculate agreement summaries and Krippendorff's alpha for all reviewer pairs. Krippendorff's alpha can be calculated for nominal, ordinal, interval, and ratio scale variables.
 #' @name calculate_agreement
-#' @usage calculate_agreement(articles, kr_method = "nominal")
+#' @usage calculate_agreement(articles, decision_col = "decision", kr_method = "nominal")
 #' @param articles a data frame that contains the screened articles, and having a name<chr> column
+#' @param decision_col variable name <chr> that defines the decision column (0/1)
 #' @param kr_method The scale of the decision variable. Can be nominal (default), ordinal, interval, or ratio
 #' @return A data frame containing agreement summaries, and the Krippendorff's alpha statistic for all pairs of screeners. invalid_decision<int> is the total number of decisions that are not 1 or 0.
 #' @examples
 #' calculate_agreement(articles)
-# TODO: use explicit namespace
 
 library(dplyr)
-library(tidyr)
-library(purrr)
-library(tibble)
-library(irr)
-source("script/functions/tidy_kripp.R")
 
-calculate_agreement <- function(articles, kr_method = "nominal"){
-    stopifnot(has_name(articles, c("name","decision")))
+calculate_agreement <- function(articles, decision_col = "decision", kr_method = "nominal"){
+    stopifnot(tibble::has_name(articles, c("name", decision_col)))
 
     # Create a nested dataframe from all unrepeated combinations of names
     name_pairs <-
         articles %>%
-        distinct(name) %>%
-        pull() %>%
-        combn(., 2) %>%
+        dplyr::distinct(name) %>%
+        dplyr::pull() %>%
+        utils::combn(., 2) %>%
         t() %>%
-        as_data_frame() %>%
-        set_names(c("name1", "name2")) %>%
-        mutate(name_pair = paste(name1, name2, sep = "_")) %>%
-        group_by(name_pair) %>%
-        nest(.key = name_df)
+        tibble::as_data_frame() %>%
+        purrr::set_names(c("name1", "name2")) %>%
+        dplyr::mutate(name_pair = paste(name1, name2, sep = "_")) %>%
+        dplyr::group_by(name_pair) %>%
+        tidyr::nest(.key = name_df)
 
     # Suppress coertion warnings which are the result of invalid decisions, info is not lost as invalid decision is returned in separate variable
     suppressWarnings(
         name_pairs %>%
             # Create tables for decision and reason separately
-            mutate(
-                decision_table = map(
+            dplyr::mutate(
+                decision_table = purrr::map(
                     name_df,
                     ~ articles %>%
-                        drop_na(decision) %>%
-                        select(-position,-reason) %>%
-                        filter(name %in% (.x %>% c())) %>%
-                        spread(name, decision, sep = "_") %>%
-                        set_names(str_replace(names(.), "name", "decision")) %>%
-                        drop_na(contains("decision"))
+                        tidyr::drop_na(!!decision_col) %>%
+                        dplyr::select(-position,-reason) %>%
+                        dplyr::filter(name %in% (.x %>% c())) %>%
+                        tidyr::spread(name, !!decision, sep = "_") %>%
+                        purrr::set_names(stringr::str_replace(names(.), "name", "decision")) %>%
+                        tidyr::drop_na(contains("decision"))
                 )) %>%
             # Drop rows that have no common articles
-            filter(map(decision_table, ~nrow(.x)) > 0) %>%
+            dplyr::filter(purrr::map(decision_table, ~nrow(.x)) > 0) %>%
             # Calculate the Krippendorff's alpha for all pairs
-            transmute(
+            dplyr::transmute(
                 name_pair,
                 # Create tidy Krippendorff's alpha output tables for all pairs
-                irr = map(
+                kr_alpha = purrr::map(
                     decision_table,
                     ~select(.x, starts_with("decision")) %>%
-                        mutate_all(as.numeric) %>%
-                        drop_na() %>%
+                        dplyr::mutate_all(as.numeric) %>%
+                        tidyr::drop_na() %>%
                         t() %>%
                         irr::kripp.alpha(method = kr_method) %>%
-                        tidy_kripp() %>%
-                        as_data_frame()),
+                        tidy_kripp() %>% # From {metamanager}
+                        tibble::as_data_frame()),
                 # Create summary statistics for all pairs
-                include_both = map_int(decision_table,
-                                       ~select(.x, starts_with("decision")) %>%
-                                           filter(.[[1]] == 1 & .[[2]] == 1) %>%
+                include_both = purrr::map_int(decision_table,
+                                       ~dplyr::select(.x, dplyr::starts_with("decision")) %>%
+                                           dplyr::filter(.[[1]] == 1 & .[[2]] == 1) %>%
                                            nrow()),
-                exclude_both = map_int(decision_table,
-                                       ~select(.x, starts_with("decision")) %>%
-                                           filter(.[[1]] == 0 & .[[2]] == 0) %>%
+                exclude_both = purrr::map_int(decision_table,
+                                       ~dplyr::select(.x, dplyr::starts_with("decision")) %>%
+                                           dplyr::filter(.[[1]] == 0 & .[[2]] == 0) %>%
                                            nrow()),
-                no_agreement = map_int(decision_table,
-                                       ~select(.x, starts_with("decision")) %>%
-                                           filter(.[[1]] != .[[2]]) %>%
+                no_agreement = purrr::map_int(decision_table,
+                                       ~dplyr::select(.x, dplyr::starts_with("decision")) %>%
+                                           dplyr::filter(.[[1]] != .[[2]]) %>%
                                            nrow()),
                 # The number of all invalid decisions for both screeners of the pair summarised
-                invalid_decision = map_int(decision_table,
-                                   ~select(.x, starts_with("decision")) %>%
+                invalid_decision = purrr::map_int(decision_table,
+                                   ~dplyr::select(.x, dplyr::starts_with("decision")) %>%
                                        c(recursive = TRUE) %>%
-                                       str_detect("1|0") %>%
-                                       magrittr::not() %>%
+                                       stringr::str_detect("1|0") %>%
+                                       `!` %>% # Not
                                        sum())
             ) %>%
         # Return a tidy data frame
         unnest(irr) %>%
-            right_join(name_pairs %>% select(name_pair), by = "name_pair")
+            dplyr::right_join(name_pairs %>% dplyr::select(name_pair), by = "name_pair")
     )
 }
