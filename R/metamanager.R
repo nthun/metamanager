0f8cc8f3f2cbdf03e33e02780a991698ba2440fb
nthun <thomassocska@yahoo.com>
2018-07-04 00:06:04 +0200

    Add documentation about package

---
+++ R/metamanager.R
@@ -0,0 +1,9 @@
+#' Functions for efficient meta-analysis project management
+#'
+#' Helps to menage files and workflow in meta-analysis projects
+#'
+#' @name metamanager-package
+#' @aliases metamanager-package metamanager
+#' @docType package
+#' @author Tamas NAGY <nagytamas.hungary@gmail.com>
+NULL
