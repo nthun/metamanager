c2dabd50fdeb23a191b13be411ca40f9d59bec9b
nthun <thomassocska@yahoo.com>
2018-07-04 17:59:13 +0200

    Add helper function to get cell-wise conversion fails. Should be undocumented in the future

---
+++ R/get_conversion_warnings.R
@@ -0,0 +1,38 @@
+#' Cell-wise warnings for conversion failiures
+#'
+#' Helper function to get conversion warnings separately for each cell
+#' @name get_conversion_warnings
+#' @usage get_conversion_warnings(df, fun)
+#'
+#' @param df data frame
+#' @param fun unquoted conversion function name without ()
+#'
+#' @return A data frame, where cells contain either NA (no problem with conversion), or the warning message as string
+#' @examples
+#' df <- tibble( a = as.character(1:5),
+#'               b = as.double(1:5),
+#'               c = as.character(c(1:2, letters[1:3])),
+#'               d = as.character(c(letters[4:5], c(3:5))))
+#' get_conversion_warnings(df, as.integer)
+
+get_conversion_warnings <- function(df, fun){
+
+    # Error handling
+    stopifnot(is.data.frame(df),
+              is.function(fun))
+
+    # Make a quiet version of the function
+    quiet_fun <- purrr::quietly(fun)
+
+    # Create a cell-wise report of conversion errors
+    df %>%
+        # This following part works but may not be optimal. Improve if possible!
+        purrr::map(., ~purrr::map(.x, ~quiet_fun(.x)) %>%
+                       purrr::transpose() %>%
+                       .[["warnings"]] %>%
+                       as.character()
+        ) %>%
+        dplyr::as_data_frame() %>%
+        # This seems suboptimal, should render empty vectors as NA earlier (but how?)
+        dplyr::mutate_all(funs(dplyr::recode), `character(0)` = NA_character_)
+}
