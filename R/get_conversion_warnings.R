e9c45d37bdc92b60b28a0180d4856cc5d93efd0d
nthun <thomassocska@yahoo.com>
2018-07-09 13:51:31 +0200

    Catch conversion failes as errors instead of warnings with purrr::safely

--- R/get_conversion_warnings.R
+++ R/get_conversion_warnings.R
@@ -9,31 +9,29 @@
 #'
 #' @return A data frame, where cells contain either NA (no problem with conversion), or the warning message as string
 #' @examples
-#' df <- tibble( a = as.character(1:5),
-#'               b = as.double(1:5),
-#'               c = as.character(c(1:2, letters[1:3])),
-#'               d = as.character(c(letters[4:5], c(3:5))))
-#' get_conversion_warnings(df, as.integer)
-#' TODO: check if the conversion function is one of as.integer, as.numeric, as.double, ...
+#' df <- tibble::tibble( a = as.character(1:5),
+#'                       b = as.double(1:5),
+#'                       c = as.character(c(1:2, letters[1:3])),
+#'                       d = as.character(c(letters[4:5], c(3:5))))
+#' get_conversion_warnings(df, as_integer)
 
 get_conversion_warnings <- function(df, fun){
 
     # Error handling
     stopifnot(is.data.frame(df),
               is.function(fun))
-
     # Make a quiet version of the function
-    quiet_fun <- purrr::quietly(fun)
+    quiet_fun <- purrr::safely(fun)
 
     # Create a cell-wise report of conversion errors
     df %>%
         # This following part works but may not be optimal. Improve if possible!
         purrr::map(., ~purrr::map(.x, ~quiet_fun(.x)) %>%
                        purrr::transpose() %>%
-                       .[["warnings"]] %>%
+                       .[["error"]] %>%
                        as.character()
         ) %>%
-        dplyr::as_data_frame() %>%
+        tibble::as_data_frame() %>%
         # This seems suboptimal, should render empty vectors as NA earlier (but how?)
-        dplyr::mutate_all(funs(dplyr::recode), `character(0)` = NA_character_)
+        dplyr::mutate_all(dplyr::funs(dplyr::recode), `character(0)` = NA_character_)
 }
