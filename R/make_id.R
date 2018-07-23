dc5f648dd48185ee05b9dca3640836492ede8fbd
nthun <thomassocska@yahoo.com>
2018-07-24 00:24:46 +0200

    Use temporary id for each article instead of title to avoid merging of duplicate titles

--- R/make_id.R
+++ R/make_id.R
@@ -13,24 +13,26 @@ make_id <- function(df,
                     identifier = c("doi","pmid","psyid")){
     # Stop if there are no valid identifiers in the data frame
     stopifnot(is.data.frame(df),
-              length(intersect(names(df), identifier)) > 0)
+              length(dplyr::intersect(names(df), identifier)) > 0)
     # Create a tbl of identifiers for merging
     id_hierarchy <- tibble::tibble(identifier = identifier,
                            id_rank = seq(1L, length(identifier), 1L))
 
     df %>%
+        # Make a temporal id so articles will be groupable after gather
+        mutate(temp_id = dplyr::row_number()) %>%
         # Gather all available identifiers
         tidyr::gather(identifier, id, dplyr::intersect(names(.), identifier)) %>%
         tidyr::drop_na(id) %>%
         # Get information about the rank of the identifier
         dplyr::left_join(id_hierarchy, by = "identifier") %>%
-        dplyr::group_by(title) %>%
+        dplyr::group_by(temp_id) %>%
         # Find the best available identifier for the article
         dplyr::mutate(best_id = min(id_rank)) %>%
         dplyr::ungroup() %>%
         # Remove all identifiers that are less important
         dplyr::filter(id_rank == best_id) %>%
         # Remove clutter and rearrange variables
-        dplyr::select(-id_rank, -best_id) %>%
+        dplyr::select(-id_rank, -best_id, -temp_id) %>%
         dplyr::select(identifier, id, source, dplyr::everything())
 }
