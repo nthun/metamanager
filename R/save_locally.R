ec69cfccd3c578d1a9c12dde7c73e23bc5b5282d
nthun <thomassocska@yahoo.com>
2018-07-30 13:08:44 +0200

    Corrected nesting variable

--- R/save_locally.R
+++ R/save_locally.R
@@ -22,7 +22,7 @@ save_locally <- function(df, local_path = NULL, nesting = NULL, postfix = NULL, 
                 !is.null(dir),
                 !is.null(nesting),
                 !is.null(postfix),
-                rlang::has_name(df, !!nesting),
+                rlang::has_name(df, nesting),
                 !(dir.exists(dir) & overwrite == FALSE))
     # Create a nested tibble
     df_nested <-
@@ -37,3 +37,44 @@ save_locally <- function(df, local_path = NULL, nesting = NULL, postfix = NULL, 
     stringr::str_glue("{nrow(df_nested)} files saved in {dir}")
 }
 
+#' Save several files locally based on a nesting variable
+#'
+#' Cut a dataframe into several pieces based on a nesting variable, and save these pieces separately in a specified folder with a specified postfix.
+#' @name save_locally
+#' @usage save_locally(df, local_path, nesting, postfix, overwrite)
+#' @param df a data frame that should be saved in segments
+#' @param local_path a full path <chr> where the files will be saved as .csv
+#' @param nesting variable name <chr> that will used as nesting variable
+#' @param postfix a string <chr> that will be appended to the end of the file name
+#' @param overwrite overwrite files <lgl>? (default = FALSE)
+#' @return No output, this function exerts a side-effect.
+#' @examples
+#' save_locally(articles, "d:/temp/screening/", nesting = "reviewer", postfix = "",overwrite = TRUE)
+# TODO: test!
+# TODO: nesting variable does not work well. Should come up with something better.
+# TODO: Make it tidyeval
+# TODO: ? use fs instead of base file operations
+
+save_locally <- function(df, local_path = NULL, nesting = NULL, postfix = NULL, overwrite = FALSE){
+    # Checking predicaments
+    stopifnot(
+        !is.null(local_path),
+        !is.null(nesting),
+        !is.null(postfix),
+        rlang::has_name(df, nesting),
+        !(dir.exists(local_path) & overwrite == FALSE))
+    # Create a nested tibble
+    df_nested <-
+        df %>%
+        dplyr::group_by(!!sym(nesting)) %>%
+        tidyr::nest()
+    # Create the directory
+    if (dir.exists(local_path) & overwrite == TRUE) unlink(local_path, recursive = TRUE)
+    dir.create(local_path)
+    # Save the screening files
+    purrr::walk2(dplyr::pull(df_nested, !!rlang::sym(nesting)), 
+                 dplyr::pull(df_nested, data), 
+                 ~readr::write_excel_csv(x = .y, 
+                                         path = stringr::str_glue("{local_path}/{.x}_{postfix}.csv"), na = ""))
+    message(stringr::str_glue("{nrow(df_nested)} files saved in {local_path}"))
+}
